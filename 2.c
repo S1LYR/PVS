fa3cd91ee824b65276870ea5ff346569b8f0f564
S1LYR <131026211+S1LYR@users.noreply.github.com>
2025-05-22 19:12:56 +0500

    2.c

--- 2.c
+++ 2.c
@@ -1,124 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <omp.h>
-#include <string.h>  // Добавляем для memcmp
 
-#define N 100000000  // 100 миллионов элементов
-#define RANGE 1000   // Диапазон значений: 0..999
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-// Последовательная быстрая сортировка
-void sequential_quicksort(int *arr, int left, int right) {
-    if (left >= right) return;
-    
-    int pivot = arr[(left + right) / 2];
-    int i = left, j = right;
-    
-    while (i <= j) {
-        while (arr[i] < pivot) i++;
-        while (arr[j] > pivot) j--;
-        if (i <= j) {
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
+int partition(int *array, int low, int high) {
+    int pivot = array[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (array[j] < pivot) {
             i++;
-            j--;
+            swap(&array[i], &array[j]);
         }
     }
-    
-    sequential_quicksort(arr, left, j);
-    sequential_quicksort(arr, i, right);
+    swap(&array[i + 1], &array[high]);
+    return i + 1;
 }
 
-// Параллельная быстрая сортировка
-void parallel_quicksort(int *arr, int left, int right, int threads) {
-    if (left >= right) return;
-    
-    int pivot = arr[(left + right) / 2];
-    int i = left, j = right;
-    
-    while (i <= j) {
-        while (arr[i] < pivot) i++;
-        while (arr[j] > pivot) j--;
-        if (i <= j) {
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-            i++;
-            j--;
+void quicksort_sequential(int *array, int low, int high) {
+    if (low < high) {
+        int pi = partition(array, low, high);
+        quicksort_sequential(array, low, pi - 1);
+        quicksort_sequential(array, pi + 1, high);
+    }
+}
+
+void quicksort_parallel(int *array, int low, int high, int threads) {
+    if (low < high) {
+        int pi = partition(array, low, high);
+        #pragma omp task shared(array) if (threads > 1)
+        quicksort_parallel(array, low, pi - 1, threads / 2);
+        #pragma omp task shared(array) if (threads > 1)
+        quicksort_parallel(array, pi + 1, high, threads / 2);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <array_size> <num_threads>\n", argv[0]);
+        return 1;
+    }
+
+    int size = atoi(argv[1]);
+    int threads = atoi(argv[2]);
+
+    int *array = malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % 100;
+    }
+
+    double start = omp_get_wtime();
+    if (threads == 1) {
+        quicksort_sequential(array, 0, size - 1);
+    } else {
+        #pragma omp parallel num_threads(threads)
+        {
+            #pragma omp single
+            quicksort_parallel(array, 0, size - 1, threads);
         }
     }
-    
-    #pragma omp parallel sections num_threads(threads)
-    {
-        #pragma omp section
-        parallel_quicksort(arr, left, j, threads);
-        #pragma omp section
-        parallel_quicksort(arr, i, right, threads);
-    }
-}
+    double end = omp_get_wtime();
 
-// Проверка отсортированности
-int is_sorted(int *arr, int size) {
-    for (int i = 0; i < size - 1; i++) {
-        if (arr[i] > arr[i + 1]) return 0;
-    }
-    return 1;
-}
-
-void benchmark() {
-    int *array_seq = malloc(N * sizeof(int));
-    int *array_par = malloc(N * sizeof(int));
-    if (!array_seq || !array_par) {
-        printf("Memory allocation failed!\n");
-        exit(1);
-    }
-
-    // Инициализация массива случайными числами
-    srand(time(NULL));
-    for (int i = 0; i < N; i++) {
-        array_seq[i] = array_par[i] = rand() % RANGE;
-    }
-
-    // Эталонная последовательная сортировка
-    double seq_start = omp_get_wtime();
-    sequential_quicksort(array_seq, 0, N - 1);
-    double seq_time = omp_get_wtime() - seq_start;
-    int seq_correct = is_sorted(array_seq, N);
-
-    printf("Array size: %d elements | Range: 0..%d\n\n", N, RANGE - 1);
-    printf("Mode       | Threads | Time (sec) | Speedup | Correct\n");
-    printf("-----------+---------+------------+---------+--------\n");
-    printf("Sequential |      1  | %9.5f  |  1.00x  |   %s\n", 
-           seq_time, seq_correct ? "yes" : "NO!");
-
-    // Тестируем параллельные версии
-    int thread_counts[] = {2, 4, 8, 16};
-    for (int i = 0; i < 4; i++) {
-        int threads = thread_counts[i];
-        
-        // Восстанавливаем несортированный массив
-        for (int j = 0; j < N; j++) {
-            array_par[j] = array_seq[j];
-        }
-        
-        double par_start = omp_get_wtime();
-        parallel_quicksort(array_par, 0, N - 1, threads);
-        double par_time = omp_get_wtime() - par_start;
-        int par_correct = is_sorted(array_par, N) && 
-                         (memcmp(array_seq, array_par, N * sizeof(int)) == 0);
-        
-        printf("Parallel   |    %2d   | %9.5f  | %6.2fx  |   %s\n",
-               threads, par_time, seq_time / par_time,
-               par_correct ? "yes" : "NO!");
-    }
-
-    free(array_seq);
-    free(array_par);
-}
-
-int main() {
-    printf("Available CPU cores: %d\n", omp_get_num_procs());
-    benchmark();
+    printf("Time: %.6f sec\n", end - start);
+    free(array);
     return 0;
 }
